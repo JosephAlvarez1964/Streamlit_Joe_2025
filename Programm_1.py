6d9f5f05f4338758846bbe885c785690832e6512
JosephAlvarez1964 <jalvarez@uach.mx>
2025-05-28 17:28:33 -0600

    Update Programm_1.py

--- Programm_1.py
+++ Programm_1.py
@@ -1,3 +1,16 @@
 # Importar libreria streamlit
 import streamlit as st
 st.title("Programa de Práctica")
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
